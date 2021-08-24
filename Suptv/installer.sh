4e3a6e62d30d74da4f89ac9806a5bec1912974fb
MOHAMED_OS <mohamed_os@gmail.com>
2021-08-24 15:42:35 +0100

    Update Script Suptv

--- Suptv/installer.sh
+++ Suptv/installer.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
-#####################################
-# wget https://raw.githubusercontent.com/MOHAMED19OS/Download/main/Suptv/installer.sh -qO - | /bin/sh
+# ###########################################
+# SCRIPT : DOWNLOAD AND INSTALL Suptv
+# ###########################################
+#
+# Command: wget https://raw.githubusercontent.com/MOHAMED19OS/Download/main/Suptv/installer.sh -qO - | /bin/sh
+#
+# ###########################################
 
 ###########################################
 # Configure where we can find things here #
 TMPDIR='/tmp'
-VERPY2='suptv_2.0.1'
-VERPY3='suptv_3.1'
 Package='enigma2-plugin-extensions-suptv*'
 URL='https://github.com/MOHAMED19OS/Download/blob/main/Suptv'
 
@@ -22,33 +25,36 @@ if [ $OSTYPE = "Opensource" ]; then
     OPKGINSTAL='opkg install'
 fi
 
+if python --version 2>&1 | grep -q '^Python 3\.'; then
+   echo "You have Python3 image"
+   sleep 2; clear
+   VERSION='3.1'
+else
+  echo "You have Python2 image"
+  sleep 2; clear
+  VERSION='2.0.1'
+fi
+
 ###################
 #  Install Plugin #
 
 if grep -qs "Package: $Package" $STATUS ; then
     echo ""
     echo "SupTV found in device..."
-    sleep 2; clear
+    sleep 1; clear
     echo "Goodbye!"
-    exit 0
 elif python --version 2>&1 | grep -q '^Python 3\.'; then
-    echo ""
-    echo "You have Python3 image"
-    sleep 3; clear
     echo "Opkg Update ..."
     $OPKG > /dev/null 2>&1
-    echo ""
-    wget --show-progress "$URL"/enigma2-plugin-extensions-"$VERPY3"_all.ipk?raw=true -qO $TMPDIR/enigma2-plugin-extensions-"$VERPY3"_all.ipk;
-    $OPKGINSTAL $TMPDIR/enigma2-plugin-extensions-"$VERPY3"_all.ipk
+    echo "Downloading And Insallling Suptv plugin Please Wait ......"
+    wget "$URL"/enigma2-plugin-extensions-suptv_"$VERSION"_all.ipk?raw=true -qO $TMPDIR/enigma2-plugin-extensions-suptv_"$VERSION"_all.ipk
+    $OPKGINSTAL $TMPDIR/enigma2-plugin-extensions-suptv_"$VERSION"_all.ipk
 else
-    echo ""
-    echo "You have Python2 image"
-    sleep 3; clear
     echo "Opkg Update ..."
     $OPKG > /dev/null 2>&1
-    echo ""
-    wget --show-progress "$URL"/enigma2-plugin-extensions-"$VERPY2"_all.ipk?raw=true -qO $TMPDIR/enigma2-plugin-extensions-"$VERPY2"_all.ipk;
-    $OPKGINSTAL $TMPDIR/enigma2-plugin-extensions-"$VERPY2"_all.ipk
+    echo "Downloading And Insallling Suptv plugin Please Wait ......"
+    wget "$URL"/enigma2-plugin-extensions-suptv_"$VERSION"_all.ipk?raw=true -qO $TMPDIR/enigma2-plugin-extensions-suptv_"$VERSION"_all.ipk
+    $OPKGINSTAL $TMPDIR/enigma2-plugin-extensions-suptv_"$VERSION"_all.ipk
 fi
 
 ######################
@@ -57,7 +63,19 @@ fi
 if [ -f $TMPDIR/$Package ] ; then
     rm -rf $TMPDIR/$Package
 else
-    echo ""
+    echo
 fi
+
+sleep 2; clear
+echo ""
+echo "***********************************************************************"
+echo "**                                                                    *"
+echo "**                       Suptv      : $VERSION                           *"
+echo "**                       Uploaded by: MOHAMED_OS                      *"
+echo "**                       Develop by : Suptv                           *"
+echo "**  Support    : https://script-enigma2.club/suptv/                   *"
+echo "**                                                                    *"
+echo "***********************************************************************"
+echo ""
 
 exit 0
