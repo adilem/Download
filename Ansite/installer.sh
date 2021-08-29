1663f17b8a930f49c13af86b231b26fda78a41ca
MOHAMED_OS <mohamed_os@gmail.com>
2021-08-29 12:56:58 +0100

    Update Script Ansite

--- Ansite/installer.sh
+++ Ansite/installer.sh
@@ -10,8 +10,8 @@
 ###########################################
 # Configure where we can find things here #
 TMPDIR='/tmp'
-Package='enigma2-plugin-extensions-ansite*'
-URL='https://github.com/MOHAMED19OS/Download/blob/main/Ansite'
+PACKAGE='enigma2-plugin-extensions-ansite'
+MY_URL='https://raw.githubusercontent.com/MOHAMED19OS/Download/main/Ansite/'
 
 ####################
 #  Image Checking  #
@@ -27,51 +27,49 @@ if [ $OSTYPE = "Opensource" ]; then
 fi
 
 if python --version 2>&1 | grep -q '^Python 3\.'; then
-   echo "You have Python3 image"
-   sleep 2; clear
-   VERSION='1.4'
+    echo "You have Python3 image"
+    sleep 1; clear
+    VERSION='1.4'
 else
-  echo "You have Python2 image"
-  sleep 2; clear
-  VERSION='1.3'
+    echo "You have Python2 image"
+    sleep 1; clear
+    VERSION='1.3'
 fi
+
+##################################
+# Remove previous files (if any) #
+rm -rf $TMPDIR/${PACKAGE}*
 
 ######################
 #  Remove Old Plugin #
-if grep -qs "Package: $Package" $STATUS ; then
-    echo
+if grep -qs "Package: $PACKAGE" $STATUS ; then
     echo "Remove old version..."
-    $OPKGREMOV $Package
+    $OPKGREMOV $PACKAGE
     sleep 1; clear
 else
     echo "No older version was found on the device... "
-    sleep 2; clear
+    sleep 1; clear
 fi
+
+echo "Opkg Update ..."
+$OPKG > /dev/null 2>&1
 
 ###################
 #  Install Plugin #
 if python --version 2>&1 | grep -q '^Python 3\.'; then
-    echo "Opkg Update ..."
-    $OPKG > /dev/null 2>&1
     echo "Downloading And Insallling Ansite plugin Please Wait ......"
-    wget "$URL"/enigma2-plugin-extensions-ansite_"$VERSION"_all.ipk?raw=true -qO $TMPDIR/enigma2-plugin-extensions-ansite_"$VERSION"_all.ipk
-    $OPKGINSTAL $TMPDIR/enigma2-plugin-extensions-ansite_"$VERSION"_all.ipk
+    wget $MY_URL/${PACKAGE}_${VERSION}_all.ipk -qP $TMPDIR
+    $OPKGINSTAL $TMPDIR/${PACKAGE}_${VERSION}_all.ipk
 else
-    echo "Opkg Update ..."
-    $OPKG > /dev/null 2>&1
     echo "Downloading And Insallling Ansite plugin Please Wait ......"
-    wget "$URL"/enigma2-plugin-extensions-ansite_"$VERSION"_all.ipk?raw=true -qO $TMPDIR/enigma2-plugin-extensions-ansite_"$VERSION"_all.ipk
-    $OPKGINSTAL $TMPDIR/enigma2-plugin-extensions-ansite_"$VERSION"_all.ipk
+    wget $MY_URL/${PACKAGE}_${VERSION}_all.ipk -qP $TMPDIR
+    $OPKGINSTAL $TMPDIR/${PACKAGE}_${VERSION}_all.ipk
 fi
 
-######################
-# Delete File In TMP #
+#########################
+# Remove files (if any) #
+rm -rf $TMPDIR/${PACKAGE}*
 
-if [ -f $TMPDIR/$Package ] ; then
-    rm -rf $TMPDIR/$Package
-else
-    echo
-fi
 
 sleep 2; clear
 echo ""
