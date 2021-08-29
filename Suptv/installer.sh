dffd8f34cc68f4b603a0f214214ae00e5fcbd92d
MOHAMED_OS <mohamed_os@gmail.com>
2021-08-29 13:52:46 +0100

    Update Script Suptv

--- Suptv/installer.sh
+++ Suptv/installer.sh
@@ -10,36 +10,36 @@
 ###########################################
 # Configure where we can find things here #
 TMPDIR='/tmp'
-Package='enigma2-plugin-extensions-suptv*'
-URL='https://github.com/MOHAMED19OS/Download/blob/main/Suptv'
+PACKAGE='enigma2-plugin-extensions-suptv'
+MY_URL='https://raw.githubusercontent.com/MOHAMED19OS/Download/main/Suptv'
 
 ####################
 #  Image Checking  #
-if [ -f /etc/opkg/opkg.conf ] ; then
+if which opkg > /dev/null 2>&1; then
     STATUS='/var/lib/opkg/status'
     OSTYPE='Opensource'
-fi
-
-if [ $OSTYPE = "Opensource" ]; then
     OPKG='opkg update'
     OPKGINSTAL='opkg install'
 fi
 
 if python --version 2>&1 | grep -q '^Python 3\.'; then
-   echo "You have Python3 image"
-   sleep 2; clear
-   VERSION='3.1'
+    echo "You have Python3 image"
+    sleep 1; clear
+    VERSION='3.1'
 else
-  echo "You have Python2 image"
-  sleep 2; clear
-  VERSION='2.0.1'
+    echo "You have Python2 image"
+    sleep 1; clear
+    VERSION='2.0.1'
 fi
+
+##################################
+# Remove previous files (if any) #
+rm -rf $TMPDIR/${PACKAGE}*
 
 ###################
 #  Install Plugin #
 
-if grep -qs "Package: $Package" $STATUS ; then
-    echo ""
+if grep -qs "Package: $PACKAGE" $STATUS ; then
     echo "SupTV found in device..."
     sleep 1; clear
     echo "Goodbye!"
@@ -47,30 +47,25 @@ elif python --version 2>&1 | grep -q '^Python 3\.'; then
     echo "Opkg Update ..."
     $OPKG > /dev/null 2>&1
     echo "Downloading And Insallling Suptv plugin Please Wait ......"
-    wget "$URL"/enigma2-plugin-extensions-suptv_"$VERSION"_all.ipk?raw=true -qO $TMPDIR/enigma2-plugin-extensions-suptv_"$VERSION"_all.ipk
-    $OPKGINSTAL $TMPDIR/enigma2-plugin-extensions-suptv_"$VERSION"_all.ipk
+    wget $MY_URL/${PACKAGE}_${VERSION}_all.ipk -qP $TMPDIR
+    $OPKGINSTAL $TMPDIR/${PACKAGE}_${VERSION}_all.ipk
 else
     echo "Opkg Update ..."
     $OPKG > /dev/null 2>&1
     echo "Downloading And Insallling Suptv plugin Please Wait ......"
-    wget "$URL"/enigma2-plugin-extensions-suptv_"$VERSION"_all.ipk?raw=true -qO $TMPDIR/enigma2-plugin-extensions-suptv_"$VERSION"_all.ipk
-    $OPKGINSTAL $TMPDIR/enigma2-plugin-extensions-suptv_"$VERSION"_all.ipk
+    wget $MY_URL/${PACKAGE}_${VERSION}_all.ipk -qP $TMPDIR
+    $OPKGINSTAL $TMPDIR/${PACKAGE}_${VERSION}_all.ipk
 fi
 
-######################
-# Delete File In TMP #
-
-if [ -f $TMPDIR/$Package ] ; then
-    rm -rf $TMPDIR/$Package
-else
-    echo
-fi
+##################################
+# Remove previous files (if any) #
+rm -rf $TMPDIR/${PACKAGE}*
 
 sleep 2; clear
 echo ""
 echo "***********************************************************************"
 echo "**                                                                    *"
-echo "**                       Suptv      : $VERSION                           *"
+echo "**                       Suptv      : $VERSION                             *"
 echo "**                       Uploaded by: MOHAMED_OS                      *"
 echo "**                       Develop by : Suptv                           *"
 echo "**  Support    : https://script-enigma2.club/suptv/                   *"
