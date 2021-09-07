36aad6c002f7404298276c6480a4dff044e94bbf
MOHAMED_OS <mohamed_os@gmail.com>
2021-09-07 17:26:36 +0100

    Update Script: Install Oscam & Ncam

--- Oscam_Ncam/installer.sh
+++ Oscam_Ncam/installer.sh
@@ -15,8 +15,8 @@ TMPDIR='/tmp'
 DIR=$(pwd)
 OSC_VERSION='11.695-emu-r798'
 ENC_VERSION='V11.9-r3'
-OSC_PACKAGE='enigma2-plugin-softcams-oscam*'
-ENC_PACKAGE='enigma2-plugin-softcams-ncam'
+OSC_PACKAGE='enigma2-plugin-softcams-oscam'
+NCM_PACKAGE='enigma2-plugin-softcams-ncam'
 MY_URL='https://raw.githubusercontent.com/MOHAMED19OS/Download/main/Oscam_Ncam/'
 
 ####################
@@ -39,41 +39,41 @@ fi
 
 ##################################
 # Remove previous files (if any) #
-rm -rf $TMPDIR/${OSC_PACKAGE} $TMPDIR/${ENC_PACKAGE}*
+rm -rf $TMPDIR/${OSC_PACKAGE}* $TMPDIR/${NCM_PACKAGE}*
 
-##################
-# Oscam Checking #
-checkoscam() {
-    if grep -qs "Package: $OSC_PACKAGE" $STATUS ; then
+################
+# Oscam Remove #
+removeoscam() {
+    if grep -qs "Package: $OSC_PACKAGE*" $STATUS ; then
+        echo "   >>>>   Remove Old Version   <<<<"
         echo
-        echo "Remove old version..."
         if [ $OSTYPE = "Opensource" ]; then
-            $OPKGREMOV $OSC_PACKAGE
+            $OPKGREMOV $OSC_PACKAGE*
             sleep 2; clear
         else
             $OPKGREMOV $OSC_PACKAGE
             sleep 2; clear
         fi
     else
-        echo "No older version was found on the device... "
+        echo "   >>>>   No Older Version Was Found   <<<<"
         sleep 1; clear
     fi
 }
-##################
-# Ncam Checking  #
-checkncam() {
-    if grep -qs "Package: $ENC_PACKAGE" $STATUS ; then
+################
+# Ncam Remove  #
+removencam() {
+    if grep -qs "Package: $NCM_PACKAGE" $STATUS ; then
+        echo "   >>>>   Remove Old Version   <<<<"
         echo
-        echo "Remove old version..."
         if [ $OSTYPE = "Opensource" ]; then
-            $OPKGREMOV $ENC_PACKAGE
+            $OPKGREMOV $NCM_PACKAGE
             sleep 2; clear
         else
-            $OPKGREMOV $ENC_PACKAGE
+            $OPKGREMOV $NCM_PACKAGE
             sleep 2; clear
         fi
     else
-        echo "No older version was found on the device... "
+        echo "   >>>>   No Older Version Was Found   <<<<"
         sleep 1; clear
     fi
 }
@@ -82,7 +82,7 @@ checkncam() {
 if grep -qs "Package: $PACKAGE" $STATUS ; then
     echo
 else
-    echo "Need to install $PACKAGE"
+    echo "   >>>>   Need to install $PACKAGE   <<<<"
     echo
     if [ $OSTYPE = "Opensource" ]; then
         echo "Opkg Update ..."
@@ -100,10 +100,8 @@ else
     else
         echo ""
         echo ""
-        echo "#########################################################"
-        echo "#            $PACKAGE Not found in feed                 #"
-        echo "#    Notification Emu will not work without $PACKAGE    #"
-        echo "#########################################################"
+        echo "   >>>>   Feed Missing $PACKAGE   <<<<"
+        echo "   >>>>   Notification Emu will not work without $PACKAGE   <<<<"
         sleep 3
         exit 0
     fi
@@ -114,37 +112,27 @@ clear
 echo "> Oscam EMU MENU"
 echo
 echo "  1 - Oscam"
-echo "  2 - Revcam_Oscam"
+echo "  2 - Ncam"
 echo "  3 - SupTV_Oscam"
-echo "  4 - Ncam"
+echo "  4 - Revcam_Oscam"
 echo
 echo "  x - Exit"
 echo
 echo "- Enter option:"
 read opt
 case $opt in
-    "1") EMU=Oscam checkoscam
+    "1") EMU=Oscam removeoscam
         if [ $OSTYPE = "Opensource" ]; then
             echo "Downloading And Insallling Oscam plugin Please Wait ......"
-            wget $MY_URL/enigma2-plugin-softcams-oscam_${OSC_VERSION}_all.ipk -qP $TMPDIR
-            $OPKGINSTAL $TMPDIR/enigma2-plugin-softcams-oscam_${OSC_VERSION}_all.ipk
+            wget $MY_URL/${OSC_PACKAGE}_${OSC_VERSION}_all.ipk -qP $TMPDIR
+            $OPKGINSTAL $TMPDIR/${OSC_PACKAGE}_${OSC_VERSION}_all.ipk
         else
             echo "Downloading And Insallling Oscam plugin Please Wait ......"
-            wget $MY_URL/enigma2-plugin-softcams-oscam_${OSC_VERSION}_all.deb -qP $TMPDIR
-            $DPKINSTALL $TMPDIR/enigma2-plugin-softcams-oscam_${OSC_VERSION}_all.deb; $OPKGINSTAL -f -y
+            wget $MY_URL/${OSC_PACKAGE}_${OSC_VERSION}_all.deb -qP $TMPDIR
+            $DPKINSTALL $TMPDIR/${OSC_PACKAGE}_${OSC_VERSION}_all.deb; $OPKGINSTAL -f -y
         fi
         ;;
-    "2") EMU=Revcam_Oscam checkoscam
-        echo "Downloading And Insallling Revcam_Oscam plugin Please Wait ......"
-        wget $MY_URL/enigma2-plugin-softcams-oscam-revcamv2_${OSC_VERSION}_all.ipk -qP $TMPDIR
-        $OPKGINSTAL $TMPDIR/enigma2-plugin-softcams-oscam-revcamv2_${OSC_VERSION}_all.ipk
-        ;;
-    "3") EMU=SupTV_Oscam checkoscam
-        echo "Downloading And Insallling SupTV_Oscam plugin Please Wait ......"
-        wget $MY_URL/enigma2-plugin-softcams-oscam-supcam_${OSC_VERSION}_all.ipk -qP $TMPDIR
-        $OPKGINSTAL $TMPDIR/enigma2-plugin-softcams-oscam-supcam_${OSC_VERSION}_all.ipk
-        ;;
-    "4") EMU=Ncam checkncam
+    "2") EMU=Ncam removencam
         if [ $OSTYPE = "Opensource" ]; then
             echo "Downloading And Insallling Ncam plugin Please Wait ......"
             wget $MY_URL/${ENC_PACKAGE}_${ENC_VERSION}_all.ipk -qP $TMPDIR
@@ -154,6 +142,16 @@ case $opt in
             wget $MY_URL/${ENC_PACKAGE}_${ENC_VERSION}_all.deb -qP $TMPDIR
             $DPKINSTALL $TMPDIR/${ENC_PACKAGE}_${ENC_VERSION}_all.deb; $OPKGINSTAL -f -y
         fi
+        ;;
+    "3") EMU=SupTV_Oscam removeoscam
+        echo "Downloading And Insallling SupTV_Oscam plugin Please Wait ......"
+        wget $MY_URL/${OSC_PACKAGE}-supcam_${OSC_VERSION}_all.ipk -qP $TMPDIR
+        $OPKGINSTAL $TMPDIR/${OSC_PACKAGE}-supcam_${OSC_VERSION}_all.ipk
+        ;;
+    "4") EMU=Revcam_Oscam removeoscam
+        echo "Downloading And Insallling Revcam_Oscam plugin Please Wait ......"
+        wget $MY_URL/${OSC_PACKAGE}-revcamv2_${OSC_VERSION}_all.ipk -qP $TMPDIR
+        $OPKGINSTAL $TMPDIR/${OSC_PACKAGE}-revcamv2_${OSC_VERSION}_all.ipk
         ;;
     x)
         clear
@@ -170,7 +168,7 @@ esac
 
 ################################
 # Remove script files (if any) #
-rm -rf $TMPDIR/${OSC_PACKAGE} $TMPDIR/${ENC_PACKAGE}*
+rm -rf $TMPDIR/${OSC_PACKAGE}* $TMPDIR/${NCM_PACKAGE}*
 rm -rf $DIR/installer.sh
 
 exit 1
