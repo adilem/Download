2bad3eb9940c57abe798cbe310df9b0bc6e71589
MOHAMED_OS <mohamed_os@gmail.com>
2021-09-08 17:51:48 +0100

    Update File and Script

--- Channel/installer.sh
+++ Channel/installer.sh
@@ -32,6 +32,7 @@ ABERTISBIN=${ASTRAPATH}/scripts/abertis
 ASTRABIN=$BINPATH/astra
 SPAMMERBIN=$BINPATH/spammer
 T2MBIN=$BINPATH/t2mi_decap
+ASTRASM=${ETCPATH}/init.d/astra-sm
 ###############################
 # Path of Config Files in Tmp #
 CONFIGpmttmp=${TMPDIR}/bbc_pmt_v6/bbc_pmt_starter.sh
@@ -43,6 +44,7 @@ CONFIGabertistmp=${TMPDIR}/${PACKAGE}/abertis
 CONFIGBINastratmp=${TMPDIR}/${PACKAGE}/astra
 CONFIGspammertmp=${TMPDIR}/${PACKAGE}/spammer
 CONFIGt2mi_decaptmp=${TMPDIR}/${PACKAGE}/t2mi_decap
+CONFIGastrasmtmp=${TMPDIR}/${PACKAGE}/astra-sm
 ####################
 #  Image Checking  #
 if which opkg > /dev/null 2>&1; then
@@ -105,12 +107,8 @@ if [ $OSTYPE = "Opensource" ]; then
     if grep -qs "Package: $PACKAGE" $STATUS ; then
         echo
     else
-        echo ""
-        echo "#########################################################"
-        echo "#              $PACKAGE Not found in feed               #"
-        echo "#      Notification Abertis DTT Channel will not work   #"
-        echo "#                    without $PACKAGE                   #"
-        echo "#########################################################"
+        echo "   >>>>   Feed Missing $PACKAGE   <<<<"
+        echo "   >>>>   Notification Abertis DTT Channel will not work   <<<<"
     fi
 elif [ $OSTYPE = "DreamOS" ]; then
     if grep -qs "Package: $LIBAIO1" $STATUS ; then
@@ -153,6 +151,7 @@ else
     wget $MY_URL/bbc_pmt_v6.tar.gz -qP $TMPDIR
     tar -xzf $TMPDIR/bbc_pmt_v6.tar.gz
     set +e
+    chmod -R 755 $TMPDIR/bbc_pmt_v6
     sleep 1
     echo "---------------------------------------------"
     if [ ! -f $BBCPMT ]; then
@@ -169,8 +168,6 @@ else
     fi
     echo "---------------------------------------------"
 fi
-chmod 755 /usr/bin/{bbc_pmt_starter.sh,bbc_pmt_v6.py,enigma2_pre_start.sh}
-
 
 if [ $OSTYPE = "Opensource" ]; then
     uname -m > "$CHECK"
@@ -189,6 +186,7 @@ if [ $OSTYPE = "Opensource" ]; then
             tar -xzf $TMPDIR/astra-arm.tar.gz
             mv $TMPDIR/astra-arm $TMPDIR/${PACKAGE}
             set +e
+            chmod -R 755 $TMPDIR/${PACKAGE}
             sleep 1
             echo "---------------------------------------------"
             if [ ! -f $SYSCONF ]; then
@@ -205,7 +203,6 @@ if [ $OSTYPE = "Opensource" ]; then
             fi
             echo "---------------------------------------------"
         fi
-        chmod -R 755 /etc/astra
 
     elif grep -qs -i 'mips' cat "$CHECK" ; then
         echo ':Your Device IS MIPS processor ...'
@@ -220,6 +217,7 @@ if [ $OSTYPE = "Opensource" ]; then
             tar -xzf $TMPDIR/astra-mips.tar.gz
             mv $TMPDIR/astra-mips $TMPDIR/${PACKAGE}
             set +e
+            chmod -R 755 $TMPDIR/${PACKAGE}
             sleep 1
             echo "---------------------------------------------"
             if [ ! -f $SYSCONF ]; then
@@ -236,54 +234,51 @@ if [ $OSTYPE = "Opensource" ]; then
             fi
             echo "---------------------------------------------"
         fi
-        chmod -R 755 /etc/astra
-
     fi
 elif [ $OSTYPE = "DreamOS" ]; then
-    #if grep -qs -i 'aarch64' cat "$CHECK" ; then
-        #echo ':Your Device IS AARCH64 processor ...'
-        #echo
-        if [ -f $ASTRACONF ] && [ -f $ABERTISBIN ] && [ -f $SYSCONF ] && [ -f $ASTRABIN ] && [ -f $SPAMMERBIN ] && [ -f $T2MBIN ]; then
-            echo "   >>>>   All Config $PACKAGE Files found   <<<<"
-            sleep 2
-        else
-            set -e
-            echo "Downloading Config $PACKAGE Please Wait ......"
-            wget $MY_URL/astra-aarch64.tar.gz -qP $TMPDIR
-            tar -xzf $TMPDIR/astra-aarch64.tar.gz
-            mv $TMPDIR/astra-aarch64 $TMPDIR/${PACKAGE}
-            set +e
-            sleep 1; clear
-            echo "---------------------------------------------"
-            if [ ! -f $ASTRABIN ]; then
-                cp -f $CONFIGBINastratmp $BINPATH > /dev/null 2>&1
-                echo "[send (astra) file]"
-            fi
-            if [ ! -f $SPAMMERBIN ]; then
-                cp -f $CONFIGspammertmp $BINPATH > /dev/null 2>&1
-                echo "[send (spammer) file]"
-            fi
-            if [ ! -f $T2MBIN ]; then
-                cp -f $CONFIGt2mi_decaptmp $BINPATH > /dev/null 2>&1
-                echo "[send (t2mi_decap) file]"
-            fi
-            if [ ! -f $SYSCONF ]; then
-                cp -f $CONFIGsysctltmp $ETCPATH > /dev/null 2>&1
-                echo "[send (sysctl.conf) file]"
-            fi
-            if [ ! -f $ASTRACONF ]; then
-                cp -f $CONFIGastratmp $ASTRAPATH > /dev/null 2>&1
-                echo "[send (astra.conf) file]"
-            fi
-            if [ ! -f $ABERTISBIN ]; then
-                cp -f $CONFIGabertistmp $ASTRAPATH/scripts > /dev/null 2>&1
-                echo "[send (abertis) file]"
-            fi
-            echo "---------------------------------------------"
+    if [ -f $ASTRACONF ] && [ -f $ABERTISBIN ] && [ -f $SYSCONF ] && [ -f $ASTRABIN ] && [ -f $SPAMMERBIN ] && [ -f $T2MBIN ] && [ -f $ASTRASM ]; then
+        echo "   >>>>   All Config $PACKAGE Files found   <<<<"
+        sleep 2
+    else
+        set -e
+        echo "Downloading Config $PACKAGE Please Wait ......"
+        wget $MY_URL/astra-dreamos.tar.gz -qP $TMPDIR
+        tar -xzf $TMPDIR/astra-dreamos.tar.gz
+        mv $TMPDIR/astra-dreamos $TMPDIR/${PACKAGE}
+        set +e
+        chmod -R 755 $TMPDIR/${PACKAGE}
+        sleep 1; clear
+        echo "---------------------------------------------"
+        if [ ! -f $ASTRABIN ]; then
+            cp -f $CONFIGBINastratmp $BINPATH > /dev/null 2>&1
+            echo "[send (astra) file]"
         fi
-        chmod 755 /usr/bin/{astra,spammer,t2mi_decap}
-        chmod -R 755 /etc/astra
-    #fi
+        if [ ! -f $SPAMMERBIN ]; then
+            cp -f $CONFIGspammertmp $BINPATH > /dev/null 2>&1
+            echo "[send (spammer) file]"
+        fi
+        if [ ! -f $T2MBIN ]; then
+            cp -f $CONFIGt2mi_decaptmp $BINPATH > /dev/null 2>&1
+            echo "[send (t2mi_decap) file]"
+        fi
+        if [ ! -f $SYSCONF ]; then
+            cp -f $CONFIGsysctltmp $ETCPATH > /dev/null 2>&1
+            echo "[send (sysctl.conf) file]"
+        fi
+        if [ ! -f $ASTRACONF ]; then
+            cp -f $CONFIGastratmp $ASTRAPATH > /dev/null 2>&1
+            echo "[send (astra.conf) file]"
+        fi
+        if [ ! -f $ABERTISBIN ]; then
+            cp -f $CONFIGabertistmp $ASTRAPATH/scripts > /dev/null 2>&1
+            echo "[send (abertis) file]"
+        fi
+        if [ ! -f $ASTRASM ]; then
+            cp -f $CONFIGastrasmtmp $ETCPATH/init.d > /dev/null 2>&1
+            echo "[send (astra-sm) file]"
+        fi
+        echo "---------------------------------------------"
+    fi
 fi
 #########################
 # Remove files (if any) #
@@ -292,14 +287,14 @@ rm -rf $TMPDIR/channels_backup_user_${VERSION}.tar.gz astra-* bbc_pmt_v6* > /dev
 sync
 echo ""
 echo ""
-echo "#########################################################"
+echo "*********************************************************"
 echo "#       Channel And Config INSTALLED SUCCESSFULLY       #"
 echo "#                    MOHAMED_OS                         #"
 echo "#                     support                           #"
 echo "#  https://www.tunisia-sat.com/forums/threads/4208717   #"
-echo "#########################################################"
+echo "*********************************************************"
 echo "#           your Device will RESTART Now                #"
-echo "#########################################################"
+echo "*********************************************************"
 sleep 2
 
 if [ $OSTYPE = "Opensource" ]; then
