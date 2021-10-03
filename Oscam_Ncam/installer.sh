566f9c4cca7ac629f751f03f1e734a144f966538
MOHAMED_OS <mohamed_os@gmail.com>
2021-10-03 18:09:40 +0100

    Update Oscam version: 11697

--- Oscam_Ncam/installer.sh
+++ Oscam_Ncam/installer.sh
@@ -13,7 +13,7 @@
 PACKAGE='libcurl4'
 TMPDIR='/tmp'
 DIR=$(pwd)
-OSC_VERSION='11.696-emu-r798'
+OSC_VERSION='11.697-emu-r798'
 NCM_VERSION='V11.9-r3'
 OSC_PACKAGE='enigma2-plugin-softcams-oscam'
 NCM_PACKAGE='enigma2-plugin-softcams-ncam'
