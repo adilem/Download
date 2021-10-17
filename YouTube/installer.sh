57a1d13f1c29e78cd53fc9fd87d1b2373c3f4061
MOHAMED_OS <mohamed_os@gmail.com>
2021-10-17 13:52:52 +0100

    Update YouTube: version 843

--- YouTube/installer.sh
+++ YouTube/installer.sh
@@ -10,8 +10,8 @@
 ###########################################
 # Configure where we can find things here #
 TMPDIR='/tmp'
-VERSION='git832'
-GIT='3faa9f3'
+VERSION='git834'
+GIT='cc6ad50'
 PACKAGE='enigma2-plugin-extensions-youtube'
 MY_URL='https://raw.githubusercontent.com/MOHAMED19OS/Download/main/YouTube'
 
