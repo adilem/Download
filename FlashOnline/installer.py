f0af85cb6e71348ae4717b0d273b3ccec56188f2
MOHAMED_OS <mohamed_os@gmail.com>
2021-11-12 00:22:38 +0100

    Add Script To Install Multiboot FlashOnline

---
+++ FlashOnline/installer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding; utf-8 -*-
+# ###########################################
+# SCRIPT : DOWNLOAD AND INSTALL FlashOnline
+# ###########################################
+#
+# Command: wget https://raw.githubusercontent.com/MOHAMED19OS/Download/main/FlashOnline/installer.py -qO - | python
+#
+# ###########################################
+import zlib
+import base64
+exec(zlib.decompress(base64.b64decode('eNqtVW1vm0gQ/s6vmFOlA59swK4bxa5Sqae8NXKSXtKcXX9BCyxmZdjldhdKEuW/37Dg2Emc+3SID8u8PjPzzPLhN69U0gsZ94p7nQpufYDBHwOIRMz4agqlTgaHjQTlKKPw588pXF6ff708OQ6uby0rkSKHIEhKXUoaBMDyQkgNhWRco5RHmmFQqxMLtTmFRNGDsWVpeT+1AB8TqJRZxkJX0n9KqvQm2E372W/UoqDcHCTVktGKvnGmUgq5cb27mZ003304//HjuzlatI5ooeGbsTCiNwhG76feF/GV98Z5F6VVEJ0G37NyxTgcgW3ajrYe5WyVk1HXfq+1UN5JrSlX2DvlnWZEpdc8Y5zaGCZakxXFEG0H3fBgHNNmNI6BYS/nn4rlaJhesuGv8PxvTuZjvVykPsofyHxSRvdDHc9rn8wvqvDjX3qZ12k0Sqswr4swvzuye24XzzbDt3sGumL6P5KS8xs/OhbV7OOV/rm48GeLK7Gc12qWX1Xh7SQPz04fyNmkDM+ycnl7uC+JhY39vxJMRsvFhal2X6KYJnAsfvFMkPgbT4SDc+q1FHhmY/MgCxFQxwBjtKtySRwHKSUxlc6z3GC9U1QOvq4o13Yf7EvxwLKMeJ9cH5zFcPgZZoyXNdSHB8HB+DPIajoZu34Pzmi0Ft7IH/r4DuGUSZqI2muU9m5mVSApmll0lHQQzFaPLFmjbmOGq0Ri520TtuFwb7nxMrJuN56pDUQB3bbErLVjN2po9U3YKWCh1G2Ovd0om1XZH6SZd7t9rTsiVYLjfCoqG+IHDGeDpbwYFTq9muApy6jTjY8leMO4DV1dWjOllWN7Oi+8pGwCuqxY270tDjSVNBcV3WNlGSvsMAJw7Menx6cAX5JlRu0mQuZEO5vF6EO3ln3YBd/rbaJsroGGRciJPelMTVGKHAi6WO8XtXOVvCxH3StNc8cWxXoFbW0wGCDYiA5iilyJFTw+7cA3iTqYHRX8F1jwKtJY9Q6WVxjh6Aj8LYiXU3kXW1nEBO+TL+DFtPJ4mWUw+vL70H7fowMC+1pnIawg4CRvfkCIxw6CnDAeBHYL7FUp1r9OCi68')))
