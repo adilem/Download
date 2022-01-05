2fb4ebce684472b37ac7230f8b0683b7d4bab826
MOHAMED19OS <mohamed_os@gmail.com>
2022-01-05 14:29:50 +0100

    Update Version IPTVPlayer

--- IPTVPlayer/installer.sh
+++ IPTVPlayer/installer.sh
@@ -9,7 +9,7 @@
 
 ###########################################
 # Configure where we can find things here #
-VERSION='27.12.2021'
+VERSION='05.01.2022'
 DUKTAPE='duktape'
 TMPDIR='/tmp'
 PLUGINPATH='/usr/lib/enigma2/python/Plugins/Extensions/IPTVPlayer'
@@ -17,18 +17,18 @@ SETTINGS='/etc/enigma2/settings'
 MY_URL='http://ipkinstall.ath.cx/ipk-install'
 PYTHON_VERSION=$(python -c"import sys; print(sys.version_info.major)")
 ########################
-if [ -f /etc/opkg/opkg.conf ] ; then
+if [ -f /etc/opkg/opkg.conf ]; then
     STATUS='/var/lib/opkg/status'
     OSTYPE='Opensource'
     OPKG='opkg update'
     OPKGINSTAL='opkg install'
-elif [ -f /etc/apt/apt.conf ] ; then
+elif [ -f /etc/apt/apt.conf ]; then
     STATUS='/var/lib/dpkg/status'
     OSTYPE='DreamOS'
     OPKG='apt-get update'
     OPKGINSTAL='apt-get install'
 fi
-if [ "$PYTHON_VERSION" -eq 3 ] ; then
+if [ "$PYTHON_VERSION" -eq 3 ]; then
     echo ":You have Python3 image ..."
     PY3SQLITE='python3-sqlite3'
     PLUGINPY3='E2IPLAYER_TSiplayer-PYTHON3.tar.gz'
@@ -48,7 +48,7 @@ elif uname -m | grep -qs sh4; then
 fi
 #########################
 # Remove files (if any) #
-if [ "$PYTHON_VERSION" -eq 3 ] ; then
+if [ "$PYTHON_VERSION" -eq 3 ]; then
     rm -rf ${TMPDIR}/"${PLUGINPY3:?}"
 else
     rm -rf ${TMPDIR}/"${PLUGINPY2:?}"
@@ -58,18 +58,18 @@ rm -rf /etc/enigma2/iptvplaye*.json
 rm -rf /etc/tsiplayer_xtream.conf
 rm -rf /iptvplayer_rootfs
 ############################
-$OPKG > /dev/null 2>&1
+$OPKG >/dev/null 2>&1
 
 ################
-if [ "$PYTHON_VERSION" -eq 3 ] ; then
-    if grep -qs "Package: $DUKTAPE" $STATUS ; then
+if [ "$PYTHON_VERSION" -eq 3 ]; then
+    if grep -qs "Package: $DUKTAPE" $STATUS; then
         echo
     else
         echo "   >>>>   Need to install $DUKTAPE   <<<<"
         $OPKGINSTAL $DUKTAPE
     fi
 else
-    if grep -qs "Package: $DUKTAPE" $STATUS ; then
+    if grep -qs "Package: $DUKTAPE" $STATUS; then
         echo
     else
         echo "   >>>>   Need to install $DUKTAPE   <<<<"
@@ -81,15 +81,15 @@ else
     fi
 fi
 #################
-if [ "$PYTHON_VERSION" -eq 3 ] ; then
-    if grep -qs "Package: $PY3SQLITE" $STATUS ; then
+if [ "$PYTHON_VERSION" -eq 3 ]; then
+    if grep -qs "Package: $PY3SQLITE" $STATUS; then
         echo
     else
         echo "   >>>>   Need to install $PY3SQLITE   <<<<"
         $OPKGINSTAL $PY3SQLITE
     fi
 else
-    if grep -qs "Package: $PY2SQLITE" $STATUS ; then
+    if grep -qs "Package: $PY2SQLITE" $STATUS; then
         echo
     else
         echo "   >>>>   Need to install $PY2SQLITE   <<<<"
@@ -99,10 +99,11 @@ else
     fi
 fi
 ################
-sleep 1; clear
+sleep 1
+clear
 ###############################
 # Downlaod And Install Plugin #
-if [ "$PYTHON_VERSION" -eq 3 ] ; then
+if [ "$PYTHON_VERSION" -eq 3 ]; then
     set -e
     echo "Downloading And Insallling IPTVPlayer plugin Please Wait ......"
     echo
@@ -139,11 +140,11 @@ if [ -d $PLUGINPATH ]; then
         echo "config.plugins.iptvplayer.plarform=${plarform}"
         echo "config.plugins.iptvplayer.dukpath=/usr/bin/duk"
         echo "config.plugins.iptvplayer.wgetpath=wget"
-    } >> ${SETTINGS}
+    } >>${SETTINGS}
 fi
 #########################
 # Remove files (if any) #
-if [ "$PYTHON_VERSION" -eq 3 ] ; then
+if [ "$PYTHON_VERSION" -eq 3 ]; then
     rm -rf ${TMPDIR}/"${PLUGINPY3:?}"
 else
     rm -rf ${TMPDIR}/"${PLUGINPY2:?}"
@@ -160,9 +161,12 @@ echo "**  Support    : https://www.tunisia-sat.com/forums/threads/3951696/  *"
 echo "**                                                                    *"
 echo "***********************************************************************"
 echo ""
-if [ $OSTYPE = "DreamOS" ] ; then
-    sleep 2; systemctl restart enigma2
+if [ $OSTYPE = "DreamOS" ]; then
+    sleep 2
+    systemctl restart enigma2
 else
-    init 4; sleep 2; init 3;
+    init 4
+    sleep 2
+    init 3
 fi
 exit 0
