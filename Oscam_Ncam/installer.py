df2dcee34646eaedb7c09c6b536e926a47527a67
MOHAMED19OS <mohamed_os@gmail.com>
2022-01-08 10:26:31 +0100

    Update Installer.py

--- Oscam_Ncam/installer.py
+++ Oscam_Ncam/installer.py
@@ -98,7 +98,7 @@ def Install_Menu():
             try:
                 choice = str(input("Please make a choice: "))
             except EOFError:
-                break
+                continue
 
             if choice == "1":
                 Remove_Oscam()
@@ -142,7 +142,7 @@ def Install_Menu():
             try:
                 choice = str(input("Please make a choice: "))
             except EOFError:
-                break
+                continue
 
             if choice == "1":
                 Remove_Oscam()
