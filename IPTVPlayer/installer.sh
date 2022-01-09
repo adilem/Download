77d96048b159aef12f7f21d4c07349adec013533
MOHAMED19OS <mohamed_os@gmail.com>
2022-01-09 10:21:01 +0100

    Update Version: IPTVPlayer

--- IPTVPlayer/installer.sh
+++ IPTVPlayer/installer.sh
@@ -9,7 +9,7 @@
 
 ###########################################
 # Configure where we can find things here #
-VERSION='05.01.2022'
+VERSION='08.01.2022'
 DUKTAPE='duktape'
 TMPDIR='/tmp'
 PLUGINPATH='/usr/lib/enigma2/python/Plugins/Extensions/IPTVPlayer'
