6d01c23c50a35d73155a8b6573600362bf3c54be
MOHAMED19OS <mohamed_os@gmail.com>
2022-01-11 17:11:13 +0100

    small fix Script Installer Oscam_Ncam

--- Oscam_Ncam/installer.py
+++ Oscam_Ncam/installer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding; utf-8 -*-
+
 # ###########################################
 # SCRIPT : DOWNLOAD AND INSTALL Oscam_Ncam
 # ###########################################
@@ -7,6 +8,7 @@
 # Command: python <(wget -qO- https://raw.githubusercontent.com/MOHAMED19OS/Download/main/Oscam_Ncam/installer.py)
 #
 # ###########################################
+
 from zlib import decompress
 from base64 import b64decode
 exec(decompress(b64decode('eNrtWOtv2zYQ/66/4uYOk11M0uwGaOs2ATY03fbBdtG0GQYUFWiJtjlLpMCHnSDL/76jHrbkR+ZsbhYU4wc9jsd7/O54R+nJN4FRMhgzHmTXeia48wS8px5EImZ82gejJ94LS0F6+NmSKfz0ex8Go19+HJy/CUcX8Dl0nIkUKYThxGgjaRgCSzMhNWSScY1UHmmGop2SLFT1pK7xcWJv/oJKhUwh4xPhp+QPIeE19PoO4Ki4zTiTIqJKAVGgMocmijY4IpGmhMfVvJORaE6mFE7BTdg4MjI5cR2hIpKGl1RacrfrP//hxKOp8eTzly+q2XfrhZSzaUp6XpaYKeOeEhONHMrLGV2H14Rddnv+iSe7JfUAITyXkRE9CxXTOetM60z1g0CSpT9lembGRlEZCa4p1z56GJTgd1+OLoI3YskTQeIgJRjDUW78MBfqODGdwCijXAkjI9rulFBNMAC+VenTK6a0arvBgsgAAQpENp8GShNtlFuy2yEpxpXXZJXC30hK0tHFIZLjOyWXgkqxjCNjktTEbiiyw5pq4SKZ9qZUu/kETdgul+v89l4wl7otoVT8a6G4Cly1PJrRaI5rVeajJmF0ZnR7Jbh101e3kKC/Xmk4jSGn/QlkOQf35ibfB/Bt9/bWbfkTIVOi2ysnv4cySzudyt1S4Wk1s/YiI0qVnqoaea/XuefKxw2madpukO1wcztNFhPMvbMgpouAmySB3tl33VeFE6Wd+Yu7w/i9fqzisSN4xzULvOt/YFkdvxxaG6R2C5/OcODtLcVADphSWAkLPQCvcXzia56h0GzCImLrG5ynBpYMjeJCw1LIOb5hTTW6vnqVAFtel0bWrS3S8j1NxYKG+eZu5CTad7+svH16cFI26uA6NcdCJO1SeQeYgg/SHC0RZe4oeB6aE1EvpigKi/m+1KvMOFK+ZUZOrXJitPBKUw5T3UymfI82Ijf814G7Tznhjz5wOyx80Ojt0L83hFVPGFBuah2pCdZqYTQTLLJt3KK7oi5nLKHl3M6i02o1qGeQ73U4H3yEwfnw4yfemO6CVzA0qD2kDjeJz5B4YbIPl+H2ghOce08XFot8ckPLFU6fXzG9QUZbO06DsvJZaYko22xuvUsoURRSMqdAKs+h1dlYmve6YvUptLqt/lZwm7Vvu2YW+GGQMEaJLdQFcsU5C0ozfiNMg5+P1raMO1JrnV71fhPYS5hfkOSzbO7u7OrloW6jllaveGbsNI3J87+GR28/HsND4Rg+PjR4Awx+GBbPjpEbtZ3wAJh4ymTWuS+fKCfHAKdeCh4CHZnrW/QeAJ+rHfisrXYj9FK6ezH6WYh4fE3hVWeH82NsV/MN5ZsnyzreEAvuajA8xu9djd+qcI1dpDTWb/ma6QRbyj5Fzq4++ai7z/8d5u93iP04Bo+tzk9oj1xK+z/gjkIb0/Fqt3ylveaYuPx3XeeLFdZ7wLOj2h43f76iEmv/SIYhJ6n9k4m+uWFof66FoVvo3PpL1KAW3wnOXz9c8nU=')))
