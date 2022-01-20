4140d98d004e00911849049425dd109970f6adb6
MOHAMED19OS <mohamed_os@gmail.com>
2022-01-20 20:48:37 +0100

    Update YouTube: version git970

--- YouTube/installer.sh
+++ YouTube/installer.sh
@@ -10,8 +10,8 @@
 ###########################################
 # Configure where we can find things here #
 TMPDIR='/tmp'
-VERSION='git967'
-GIT='975f3aa'
+VERSION='git970'
+GIT='6db18a4'
 PACKAGE='enigma2-plugin-extensions-youtube'
 MY_URL='https://raw.githubusercontent.com/MOHAMED19OS/Download/main/YouTube'
 PYTHON_VERSION=$(python -c"import sys; print(sys.version_info.major)")
