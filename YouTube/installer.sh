d66d34105d0a4ad89e2ac333af6e9c6dba800fd3
MOHAMED19OS <mohamed_os@gmail.com>
2022-02-19 16:54:48 +0100

    Update YouTube: Version git1002

--- YouTube/installer.sh
+++ YouTube/installer.sh
@@ -10,8 +10,8 @@
 ###########################################
 # Configure where we can find things here #
 TMPDIR='/tmp'
-VERSION='git1001'
-GIT='dd7ec72'
+VERSION='git1002'
+GIT='891cf76'
 PACKAGE='enigma2-plugin-extensions-youtube'
 MY_URL='https://raw.githubusercontent.com/MOHAMED19OS/Download/main/YouTube'
 PYTHON_VERSION=$(python -c"import sys; print(sys.version_info.major)")
