1b70be69927360d9f674379f84158dfa552fa379
MOHAMED19OS <mohamed_os@gmail.com>
2022-02-20 13:34:24 +0100

    Add Plugin HasBahCa

---
+++ HasBahCa/installer.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+# ###########################################
+# SCRIPT : DOWNLOAD AND INSTALL HasBahCa
+# ###########################################
+#
+# Command: wget https://raw.githubusercontent.com/MOHAMED19OS/Download/main/HasBahCa/installer.sh -qO - | /bin/sh
+#
+# ###########################################
+
+###########################################
+# Configure where we can find things here #
+TMPDIR='/tmp'
+VERSION='1.0'
+PACKAGE='enigma2-plugin-extensions-hasbahca'
+MY_URL='https://patbuweb.com/HasBahCa/'
+
+####################
+#  Image Checking  #
+
+if [ -f /etc/opkg/opkg.conf ]; then
+    OSTYPE='Opensource'
+    OPKGINSTAL='opkg install'
+    OPKGLIST='opkg list-installed'
+    OPKGREMOV='opkg remove --force-depends'
+elif [ -f /etc/apt/apt.conf ]; then
+    OSTYPE='DreamOS'
+    OPKGINSTAL='apt-get install'
+    OPKGLIST='apt-get list-installed'
+    OPKGREMOV='apt-get purge --auto-remove'
+    DPKINSTALL='dpkg -i --force-overwrite'
+fi
+
+##################################
+# Remove previous files (if any) #
+rm -rf $TMPDIR/"${PACKAGE:?}"* >/dev/null 2>&1
+
+######################
+#  Remove Old Plugin #
+if [ "$($OPKGLIST $PACKAGE | awk '{ print $3 }')" = $VERSION ]; then
+    echo " You are use the laste Version: $VERSION"
+    exit 1
+elif [ -z "$($OPKGLIST $PACKAGE | awk '{ print $3 }')" ]; then
+    echo
+    clear
+else
+    $OPKGREMOV $PACKAGE
+fi
+
+###################
+#  Install Plugin #
+echo "Insallling XcPlugin Forever plugin Please Wait ......"
+if [ $OSTYPE = "Opensource" ]; then
+    wget $MY_URL/${PACKAGE}_${VERSION}_all.ipk -qP $TMPDIR
+    $OPKGINSTAL $TMPDIR/${PACKAGE}_${VERSION}_all.ipk
+else
+    wget $MY_URL/${PACKAGE}_${VERSION}_all.deb -qP $TMPDIR
+    $DPKINSTALL $TMPDIR/${PACKAGE}_${VERSION}_all.deb
+    $OPKGINSTAL -f -y
+fi
+
+#########################
+# Remove files (if any) #
+rm -rf $TMPDIR/"${PACKAGE:?}"*
+
+sleep 1
+clear
+echo ""
+echo "****************************************************************************************"
+echo "**                                                                                     *"
+echo "**                            HasBahCa   : $VERSION                                         *"
+echo "**                            Uploaded by: MOHAMED_OS                                  *"
+echo "**                            Develop by : Lululla                                     *"
+echo "**     Support: https://www.linuxsat-support.com/thread/151869-plugin-hasbahca/        *"
+echo "**                                                                                     *"
+echo "****************************************************************************************"
+echo ""
+
+if [ $OSTYPE = "Opensource" ]; then
+    killall -9 enigma2
+else
+    systemctl restart enigma2
+fi
+
+exit 0
