9de8da6057074475a15ec6a6814e2ac459683b41
MOHAMED19OS <mohamed_os@gmail.com>
2022-02-25 09:28:43 +0100

    Update HasBahCa: Version 1.2

--- HasBahCa/installer.sh
+++ HasBahCa/installer.sh
@@ -10,7 +10,7 @@
 ###########################################
 # Configure where we can find things here #
 TMPDIR='/tmp'
-VERSION='1.1'
+VERSION='1.2'
 PACKAGE='enigma2-plugin-extensions-hasbahca'
 MY_URL='https://patbuweb.com/HasBahCa/'
 
