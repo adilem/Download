2a8a6170abda1fbdcf4609b6a5f306cf977c2272
MOHAMED19OS <mohamed_os@gmail.com>
2022-03-04 11:37:07 +0100

    Update Channel: 04-03-2022

--- Channel/installer.sh
+++ Channel/installer.sh
@@ -12,7 +12,7 @@
 TMPDIR='/tmp'
 PACKAGE='astra-sm'
 DVBSNOOP='dvbsnoop'
-VERSION='2022_02_28'
+VERSION='2022_03_04'
 MY_URL='https://raw.githubusercontent.com/MOHAMED19OS/Download/main/Channel'
 
 ########################
