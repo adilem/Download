99e85a5c5c1a527b6df65ae423ca35b6387dda18
MOHAMED19OS <mohamed_os@gmail.com>
2022-04-08 05:20:05 +0100

    Update Script install IPtoSat

--- IPtoSAT/installer.sh
+++ IPtoSAT/installer.sh
@@ -22,8 +22,6 @@ VERSION=$(wget $MY_URL/version -qO- | cut -d "=" -f2-)
 if [ -f /etc/opkg/opkg.conf ]; then
     STATUS='/var/lib/opkg/status'
     OSTYPE='Opensource'
-    PKGEXP3='exteplayer3'
-    PKGGPLY='gstplayer'
     OPKG='opkg update'
     OPKGINSTAL='opkg install'
     OPKGLIST='opkg list-installed'
@@ -31,15 +29,35 @@ if [ -f /etc/opkg/opkg.conf ]; then
 elif [ -f /etc/apt/apt.conf ]; then
     STATUS='/var/lib/dpkg/status'
     OSTYPE='DreamOS'
-    PKGBAPP='gstreamer1.0-plugins-base-apps'
     OPKG='apt-get update'
     OPKGINSTAL='apt-get install'
     OPKGLIST='apt-get list-installed'
     OPKGREMOV='apt-get purge --auto-remove'
     DPKINSTALL='dpkg -i --force-overwrite'
 fi
-#########
+######################
 $OPKG >/dev/null 2>&1
+
+###########
+# install #
+
+install() {
+    if grep -qs "Package: $1" $STATUS; then
+        echo
+    else
+        echo "   >>>>   Need to install $1   <<<<"
+        echo
+        if [ $OSTYPE = "Opensource" ]; then
+            $OPKGINSTAL "$1"
+            sleep 1
+            clear
+        elif [ $OSTYPE = "DreamOS" ]; then
+            $OPKGINSTAL "$1" -y
+            sleep 1
+            clear
+        fi
+    fi
+}
 
 ##################################
 # Remove previous files (if any) #
@@ -58,67 +76,13 @@ fi
 #####################
 # Package Checking  #
 if [ $OSTYPE = "Opensource" ]; then
-    if grep -qs "Package: $PKGEXP3" $STATUS; then
-        echo "$PKGEXP3 found in device..."
-        sleep 1
-        clear
-    else
-        echo " Downloading $PKGEXP3 ......"
-        echo
-        $OPKGINSTAL $PKGEXP3
-        sleep 1
-        clear
-    fi
-    if grep -qs "Package: $PKGGPLY" $STATUS; then
-        echo "$PKGGPLY found in device..."
-        sleep 1
-        clear
-    else
-        echo " Downloading $PKGGPLY ......"
-        echo
-        $OPKGINSTAL $PKGGPLY
-        sleep 1
-        clear
-    fi
-
+    for i in exteplayer3 gstplayer; do
+        install $i
+    done
 elif [ $OSTYPE = "DreamOS" ]; then
-    if grep -qs "Package: $PKGBAPP" $STATUS; then
-        echo " $PKGBAPP found in device..."
-        sleep 1
-        clear
-    else
-        echo " Downloading  $PKGBAPP ......"
-        echo
-        $OPKGINSTAL $PKGBAPP -y
-        sleep 1
-        clear
-    fi
+    install gstreamer1.0-plugins-base-apps
 fi
 
-if [ $OSTYPE = "Opensource" ]; then
-    if grep -qs "Package: $PKGEXP3" $STATUS; then
-        echo
-    else
-        echo "Feed Missing $PKGEXP3"
-        echo "Sorry, the plugin will not be install"
-        exit 1
-    fi
-    if grep -qs "Package: $PKGGPLY" $STATUS; then
-        echo
-    else
-        echo "Feed Missing $PKGGPLY"
-        echo "Sorry, the plugin will not be install"
-        exit 1
-    fi
-elif [ $OSTYPE = "DreamOS" ]; then
-    if grep -qs "Package: $PKGBAPP" $STATUS; then
-        echo
-    else
-        echo "Feed Missing $PKGBAPP"
-        echo "Sorry, the plugin will not be install"
-        exit 1
-    fi
-fi
 ###################
 #  Install Plugin #
 
