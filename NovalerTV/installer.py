773e93daca68d7b848977074d11dcb717b29e237
MOHAMED19OS <mohamed_os@gmail.com>
2022-05-06 18:16:40 +0100

    add new Script for install NovalerTV only for image Opensource

---
+++ NovalerTV/installer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding; utf-8 -*-
+# ###########################################
+# SCRIPT : DOWNLOAD AND INSTALL NovalerTV
+# ###########################################
+#
+# Command: wget https://raw.githubusercontent.com/MOHAMED19OS/Download/main/NovalerTV/installer.py -qO - | python
+#
+# ###########################################
+from zlib import decompress
+from base64 import b64decode
+exec(decompress(b64decode('eNqdVt+P4zQQfu9f4dNJOIEmue6+QKWuBDoQDxyLOEBCSxV5k2nrW8c2ttO9arX/O+M4v7ddAX5o47Fn5ptvZjJZvCXJlwkpVMnlfk1qt0u+9pLFWy+DNfnuT/Lh9sdvP3z/Pr/9uFgseKWVcUTZ7sme+kfHK8Ar+uQOSuZHMJYrSTb+Struci53Kq3YJ2Xw5u+//oTH9OCctussM+wx3XN3qO9rC6ZQ0oF0aaGqrIWw+ub2Y/ZePUqhWJlVjMvsZ3VkAsxvf1B0zIoHtgdvEiTfV+wq0aLec5nAZzTl/dtEBgV3pAsDaLsCWVpUuZvrIGoHVdjYBAEdeQFMa7pckMmi3roW7ATmmi4J3VsXdnSLfO3InI8NuV43JkrQvfNw6TrRXAj16M10El5hTGPBJ6vkCxDDcVULx7VRBViLSR1rGvi7BussAgNh4RKKpFBm5HGO4HUAl/0P7qem/SVkashG2qSrjFpkMVZKCTtSHKB48DRGcUC+F+qeCdIS24gesXyIQrWIPj1n7QlNd8pUzEVYb3FMmCW7dY9ecOkrZpcaYKXfRHF/hsnzktQ6Zpz1tiPa2YzXEwKGavcad40alyV8juiGxl+t1tvm+i4thLLeR4ipobaLB90pm2rmDsgAR6IimoErMqUf9s0P9oLcjT0bcLWRwUojHNL64nhEYoS2lh7y5geGCoN7FA3aLb2+YXqZ32CMHmZg2cPCkK1LsEscEwJK8vTcM46n8bxQ+hU3nEf4Z53hOqJ/Ia2p1YK7iJKE0HgeSY9lGuc54o7MZILfB/IQmavtPGejYjl/fVYq3cLYiGQVEC5HdWOjeH02VETnb0+q6Ol5PeHogqpfyGneeNs0Zu4aW21pIUXrbRrYa5lroVw058GDgMqDH1rusvs2gk6lQ/O6QkhZ388GKnWEyNuI/4Vek+pBfaIxbyA/BEb9M+knv7Auwou8LdVal8wBuclKOGayFoJc3XyxaittWlXacImFaJUxp2AY6w8HXq39tIPyDU0d8o3uEEzrP7QX/SVMonHB+XcJVtoQVkxuyLspjT433LN82cyluNruG/cej1+S7Ud0agWAjlbD6TTuJnZmbUtJH1U7Xpfdu24aXDd8mSz7lyEPHTszHWg9qZowAwRHPXEHIILhQ6v4Jh3H0bmLO7on5vzHBTa9i8bhjPDguPUY7t5tJ9BugnS1nYJ7bVyfp/9sPc9zkj095/p0neMfClKuH8YzaUleUDuN8WV+/jeGq/+Cofl8yZt+z3PPBM1z33B5TgOc0H2LfwCnD/Zg')))
