c58f6b11a9cb6793fd87fccb1ecb24ef1e95ffd3
MOHAMED19OS <mohamed_os@gmail.com>
2022-07-18 19:24:27 +0100

    Add New File For Oscam & ncam

---
+++ Emu/installer.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# code: BY MOHAMED_OS
+
+# ###########################################
+# SCRIPT : DOWNLOAD AND INSTALL Emu
+# ###########################################
+#
+# Command: python -c "$(wget https://raw.githubusercontent.com/MOHAMED19OS/Download/main/Emu/installer.py -qO -)"
+#
+# ###########################################
+
+from zlib import decompress
+from base64 import b64decode
+
+exec(decompress(b64decode('eNq9F9ty3DT03V+huBNsM+t1LsDAli3TXAqdoQ2TtEAmm/F4bXnXxJaMJG+a2e438A18Ac/8A/wDn8LRxbK9TUvgAWVjS+d+kY6OH+1EDWfRvCBRfSeWlDiPUPhxiFKaFWQxQY3Iw88lBOAaho4uJ+jF2TdPX5yexGcXjlNUNWUCUe7kjFYoSwQWRYWRgbdrx3HqJL1JFhhNkYdJsaiSg7Aum0VBQk5zkSYVDz2H0FsgaLnGsPQDx3l9/q1kWwpR80kUseR2vCjEspk3HLOUEoGJGKe0ioxl+1+cXUQn9JaUNMmiKgH/zjhoiF/Cw3OUNyVl3DkGse5s7/Dwaq9ykRyAKnHCkI9JFjjnHf7x4b4keYQYzgDLGGWB83Uff6DxC4YxQX7NaIo5D5yjPs1nmmZeNhj56ZIWKQ6cyz7Foaa4w2UJsfALklOIgJPhHM0TQjDzg4kjLaV8zO+4wJXvKZO9QIFrVhDhH/UXzHVdJ+4GMkPN4d+ZxWYVR/qF3kq8fMwcPY/jIEaRYptZZGSQCorQJfzJt0JGTmTVwext/Fa9lRLHMkX2DTaOEAR96rp92931Bj0nXCRlOSPuOKesSoR/HAxoYPYDKwRsAzS/Q+tNt0GB2//z179+/+WP39Qr6Ak5GqGhHB+225gLlsu957u7WbhbhbuXaPdHNwjG7KeGC//g06BNB2S4qoXJIm+zUouCEg4ZLQveIRXudlmUGL1iDZ7YCGjNdikHuOx/FYDlx0tKOUYJMVLR1XoTrjfXaIIGToxarVd719083L8OhhGVQxsE5hWkboQfjJu6lnvKEhR5S0OoAKpW3mRgown8jICtO9LWC1ziFOoAwYjmSCzB7FVSlMkcPG5jsrPTi/55F31rG5zkgjTYAhkWDSPGHhP15xUUkfYMLEo6T0oE20M0fISaWlaOEVitNgwAiJ3iN7A9ONjhGC/h/NSJWI7xG0gU970IizSi9c1CPaCckNwLOq+1DlmGolXCorKYa2IN9yydtkHSSXS7fBJleBWRpizRwZOP9jtyY5+lN+uePLJNwnBFVxiFIUQyxWGGa0hyzwLrqeQo6huNwSXHH/Qm+7A3SS3CBRb/xqGW5cM+tVR1wxbSq6QRNNQ+vsenDM81xuyPB2XSbJ/jJU5v4u/0XeSbO8mk+RbuFNismPg6DAFKOMq7oEG8UQEFl8tjkY8ZTrKyIJj7wfBwwOZSZFBLEia4FOt7RuXE2yKWAyIRa8FTzXml+QuS4Te+h4DnWhamooYDy+uyEEbvO5KkiXASK2mhlfquQmNlS2nCcD9dL9CydJk4trfrc7iffKnFuNVLiYqkdytzu95EK8xU/sKfz0K4H+CWrAEMs7QRKMyQO3VRmB+EXlsg4NIfqWjI2guhBteZjoE3Izaf52qj+CSp2iwW20nu4bbuTdAPP6uxVy0UT3DPwakTzo1q2Ve0qTdVybnfAg/OV9qw8pN+8pWkexTYK+0JDHitNy8xzsBOQe3hWm9aiQBH6EsYWzfC5bC8Dpx+DEzWIuv8sHYGnS+DiqvqdFLBRl0PL62n7kTeXFT2WdaW9nyNhrRHmpY8gPS4JzbkTf0AlpM+C8MreK0O/onptDPpgWqe9Tjeq8WybOyswqSB8OWyKWth/tMAIdWidgqgyUHooqlffR/3Mf6pBctuthNxBPCXAwnIPwHYubKtL8N/1oEHMlqThjvyf0r3f4hQ6/LAAbVoW2XH2e5WVGfVNdumx5INy+mL17bV1J8EaLLdLPbESSt1fm1P1XaESTW+wXdQnzV+lch2fyoDOYZqaPv+9oB5KoqerMSKVJd43wv75aKtn9OW/J5M9WiI/tKRUFMi4eDbw67UGOu60mDl6MIYrTcx/CAc4x6rLZGyPr/3wugPpWw0vDGMpUGvL1N9NUQjjmXxhc+GKXwVxbGssnHsaid1yR1m+G/n0CpE')))
