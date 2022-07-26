d375705c2b3cee9d48cf327b45a5b0393b998688
MOHAMED19OS <mohamed_os@gmail.com>
2022-07-26 20:35:26 +0100

    Update channel file

---
+++ Channel/installer.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# code: BY MOHAMED_OS
+
+# ###########################################
+# SCRIPT : DOWNLOAD AND INSTALL Channel
+# ###########################################
+#
+# Command: wget https://raw.githubusercontent.com/MOHAMED19OS/Download/main/Channel/installer.py -qO - | python
+#
+# ###########################################
+
+from zlib import decompress
+from base64 import b64decode
+
+exec(decompress(b64decode('eNrNV11y2zYQfucpttK4JFOR1E8sK06dTOxk0swkcSdpJvXEGYYiQYk1CTIAKNm1dYaeoSfoc+/Q3qFH6QIkJVI/jvPQmcIeEFh8WCx2F7ur9jdOzpkzjqiTXYlpSrU2WPcs8NMgopNDyEVojSQF6UgjcHwGh/Dq9Icnr549dU/falrI0gRcN8xFzojrQpRkKROQsYgKpFJfRMi1gAWeICJKSAWq5lo5T3k1Ykua8FgYxUTTlARxyrh2AkfQOu8OBh+6SQtkw6WYeAwMQgNTe7NafzjoSUgbGQa4yljKTO15fb1frE8YIRSMjKU+4dzUjuuYYYEZxzkBw5+mkU9M7ayOGBSIKxLH6RyMiIapiSK/e/MSUfpUiIwfOg7z5vYkEtN8nHPC/JQKQoXtp4lTarT34PSt8zSd0zj1Aifx0CwnU49SEju6plFkfbRUmo1TAw+RZyE55XaWZoQamlSIPp8QAdeLGWEc9Q/W51MLbsDPBVgBtI5aYIV9S7fDlCWeMFBO07QZ8QLDtLlgUSY5awEJYSyPZ4Z5qPjiKfyKC5IYutK4biqysrZxXJ+wVqulQceSzcaPDUWTlGpcTO2qXxI0HcDGr3OzxDsApwDncir/Qa7oNvY3JUIONTUH1dly1019INu5ROigK4pcOVT7cK5blq5WFafGYEn4JFmfY4cH3qgttgaf1BV1/Ohy3BzoFQEqckGQHw011AH02CO9ocbW9QLHLygXXhyf01ZlpBO00C85F8agazbwOHrPIoHOBOMrNPrqeSKn64Xx9+///PnbX3+oj3m9qLE87sBJB86wN7dJYqCLSXcIpb8Zrb3A2kusvTPY+7llVsL0983KVaLEm5DKUxjBgECVW3piapPLiAtu6A4RvpNmFxPVoe/TUK/2+1PiXxiZ518gn002yrt1uQ1iZGZFhYbwYd/I55vhVXE098GKl45dMdvh3AGJiViKHIXbxSU0miRe34m9hARjvUSXz4GRJJ2RHUgFJPGXGN+T97kL3xJ4V7a2mN2Jq8LdmSnzgii9G98S+gXWIr8cp5cIv0zi2/g2caUJZZCUBlS7qmClVfZsuORaAFOelGcynsIjJyAzh+ZxDP1H3/ZKkWVDRwIJjCh80D10H8/iid4BPZiNOU3TTP+44l6eWjryxcSEIwz/Xb2JuCWQ1tvqdT/Chp/rxWtCAvRykULp/EiTf7j4Pbblu97gJdvysaNg8sWbtwmVFneuDlk9KLxUqV5E+9MgYmgdkWT6SunrRvaLFMbdMb7GPHNl8nOvFzamdnvy65K1Sprbzf9VHAo5qqetNSNrlVyxukG1PaFBFWiRAGWuxfv+iDbhBN57kVAx1FbterGp4TOl1Teqf75Sa02VKhVbn5HLHa+B2bgD9bvMsWaoCiG7CINfpRHwOIQrxYZoGfRjX14bjVd3dtuPU06M/5ktV8pcMqy0egoWyPIKq6te/8Du4l/PmZOxg1xnWKfJgMmItPjjBOvXo+6Wp74zWCi9F/pWAQiF8EVcJC2MAOw7fV21JSdG7DCigdIvJbLEQwpLSOJiyPLyWOD2sExJHYl+9e7lTy9evnj9zNwMFZnHeYOIyiObsNCeYxGAF6aEG1hanGMt3wYvCDBByiD2ZEyYiDi0221tq0xYRPaGBwcH/d5wDZB4l1sX51/aPd+xO8pm9+08yFxcx7XRYDQadkfQ6++P+g96/S1g4WcuK9D3uw+GMDoYjLrVxiZsXoMN9/cHw+0wMXcZ8a8w+iK4iwrb+Qxuqw1URij6sphpWmY9h23itZVZbz+E+1i6CO54hSHvdtLGJm1LKmzwWb2srQJjGKuyYP0ajV8S9SP8Kb67XZfvQDc92N+vCeUxf1r8mskpFlGG+eH+x7oRinVMqh5LZgexvqGDbVfaea017TTuxpKl0rZfTtlrKU8SZfy/k0Zy3yHO7cpet/+mxmuZiscEy+jBw4hGAobLuq0ebepo9cWAiAEMUyj+Vi8LP1WboW5cV5rQdZV+XFdWaq5b6qgo25pF27+qUM/Q')))
