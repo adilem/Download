ae526aa717913ad5689dcb214af9371f4d876457
MOHAMED19OS <mohamed_os@gmail.com>
2022-08-23 17:13:38 +0100

    Update Script install Channel

--- Channel/installer.py
+++ Channel/installer.py
@@ -1,15 +1,166 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-# code: BY MOHAMED_OS
+# code BY : MOHAMED_OS
 
-# ###########################################
-# SCRIPT : DOWNLOAD AND INSTALL Channel
-# ###########################################
-#
-# Command: wget https://raw.githubusercontent.com/MOHAMED19OS/Download/main/Channel/installer.py -qO - | python
-#
-# ###########################################
+from __future__ import print_function
 
-from zlib import decompress
-from base64 import b64decode
 
-exec(decompress(b64decode('eNrNWGty2zYQ/s9TbOVJQaYSqUcsK0qdTl5NM5PUmTSZxBNnGIqEJNZ8BQAluYrO0DP0BP3dO7R36FG6AEiKlOXY+VfKQwLYD4vlYvHt0gffODlnziRMnOxCzNPEOIDO7Q74aRAmszHkYtoZyREcxzEKD09hDC9Ofnrw4slj9+QXw5iyNAbXneYiZ9R1IYyzlAnIWJgIHE18EaJWQ7CLsQF4KXzOoiic2Ix+yikX5ZxXutuW4jSjySU8ZSxlJfrNq+dPZL8NP71+/VI1DbryaSbgmUKooUuL9q9arb1Xo36/wBNUhDEt55Z9o+invGyxakx4bBpG1DCU66KUceMRHEPrrDsYvO/GLWkXoCiiHgOTJoFlvNrK7w16EnKACgOUSlss42ld3tfyGaM0ATNjqU85t4yHdcxQYyZRTsH052noU8s4rSMGGnFBoyhdghkm09RCk9EViCJzITI+dhzmLe1ZKOb5JOeU+WkiaCJsP42dIhR6d09+cR6nyyRKvcCJPYynR3MvSWjkEFQX0ClMZJeZlt6RBFc7rvxoY9fEdaUk5Ta/4ILGJlG+IZYaVgFlPqx3WKvVMqDdkZeNDxv0JUfKtu7a5b0aMAiAjU/nc4V3AE4AzmRX/oGUEBvvnwuEbBqqD+pmy1mf6w15nUkEAaJGpGSs5mGfdDpESZWmRqMa+ChVn+ENF/ysptgGfFSvSPBBZLvZIOUAlMN6QD4M9FAbMLaOScONrfUG288SLrwoOkta9jRlsSfMR5bNfs25MAddq4HH1lsWCtx2mFzAerNlANS03pj//PHvX7///ad6WOtNTeXDNjxqwynerX2WmLjzNhdsKsPAbN0KOrfizq1TuPWuZZXG9A8tqwiiMPZmtIwhRpFzEhkwmSfmNl2FXHCTOFT4Tpqdz9QNozSZknK+P6f+uZl5/jnqKdQsMbBBMgBOXXjMQZLQ09E7IufEAo/DVGMVmUgOwvjkECYwRRLzgihMKC/NKq9wqmH4eh4TXC5jkpd66THZARcTGLWnYRLgrpRGtrWS91oVyujKxOiyxh+k28LMRLfirBdvnr9+9vzZz0/26K35qlBaehNPe2l1RdAa/QlPZ8GQJllvFpRx5HFS7iryQxEgBdz2gsCdoyvwiDcMIG+QMToPZkgYpA3kRfpbGEWec2h3wXzX692D52GSr2A1GrrDO/eALca97sDuWvAUdyp1+t1eF/968GPI6DRdOUpK6mvzLE04RXsLIjfRnK0ct+YcZSVMbZdp2QGV6cwkKsE11Ck/yVmlf5WsyCxVYpAxQbcO08FMpBi0XKofA74ytWWzoaVMNPuVSO7VuUtPR4t5mpQRHNCIiuoIYMjsDX+ahLPY6zuRF9NgUg82hDMapwt6BVLbGV2n+HaEgzfRWwBvqtYWixtpVbgbK2VeEKY301tAr1Et8tUEg/G2vYqjL+lt4ootlOlRbqCmQdTtehPKRMhlylXTPAw9z+E+xp/gTiElNbyU76DVXbOd1lwmXKOMlAZ57qRaSXiQZzIhw30noAsnyaMI+ve/7dVOh6Q+CUTie0/Ugh0ey2MdLCY8SdOMfGjSz3KOJRAme1Ey7/lsD0FdkfLr1zYP3ccLH+vNz5QGmH1EiuaoRIZj8ofC7/GqMtBePqzSElokc5P1JaNS/c7lIhULyrfZVi3+PAgZ7ruIM7J1+m74+Los4u4EqTjPXFlQueuNjTnCnv1WqdbUbO0Pra/UoW0pieMGVRaWB2Uth18B6NEHSVBWCzgARWmHrniJc5F733qhUIWAra715rLzT5XDX6n7063Ha3YsZ1RA5xNqueH7IVG2ofmSKpcXlbetc/pXOms32ePGYZj78tVxb+tnwfajlFPzf7jVW6dWKkvvnkAHZFWPRX2vf2R38ddzlnTioN4Ffh5ItmZU7vwPMaat4+4eNriST+qVlCQmNMIXkeYkJAn23aVa6nLZQxIqvyxwhMU0dpEvvTySpcO0SN3XljuZx3ljEN1HL8Om9hIrWqpLN6yTz/Db9wCwjMEPLslzDwpOPjg4MPbahPzbGx4dHfV7wx1A7K32CpfXzV5eMTvMFnfsPMhclKNsNBiNht0R9PqHo/7dXn8PWPiZyzT6TvfuEEZHg1G3nNiELWuw4eHhYLgfJpYuo/4FsgWCu+iwK4/C/uOwTVw7e7aN9hrE2JekqhhWXFPmn3rmq5i50qQ+62v0rogaQ7sB6aZHh4c3sF6HxDX2FyCjkTUTT/73QKVNFi+OInke4jDjuwlTr5xLuGm9v/MBjo/V3C8lzb0J7ipnrTdVPdF0lh5U7mqrJa92WgnddVttq3hEaQaDe2ESChhW9VT9INbR6olcIet0+bUERUGmaib0ietKi1xXuoO4rqygXJdoXbqcapY8/wHW3XoR')))
+from datetime import datetime
+from sys import version_info
+from os import system, path, remove, chdir, chmod, uname
+from re import findall, MULTILINE
+import tarfile
+
+if version_info.major == 3:
+    from urllib.request import Request, urlopen, urlretrieve
+    from urllib.error import URLError, HTTPError
+else:
+    from urllib2 import Request, urlopen, URLError, HTTPError
+    from urllib import urlretrieve
+
+
+# colors
+C = "\033[0m"     # clear (end)
+R = "\033[0;31m"  # red (error)
+G = "\033[0;32m"  # green (process)
+B = "\033[0;36m"  # blue (choice)
+Y = "\033[0;33m"  # yellow (info)
+
+
+URL = 'https://raw.githubusercontent.com/MOHAMED19OS/Download/main/Channel/'
+
+
+def banner():
+
+    system('clear')
+    print(B)
+    print(r"""
+ ,-----.,--.       ,---.          ,--.  ,--.       ,--.
+'  .--./|  ,---.  /  O  \ ,--,--, |  ,'.|  | ,---. |  |
+|  |    |  .-.  ||  .-.  ||      \|  |' '  || .-. :|  |
+'  '--'\|  | |  ||  | |  ||  ||  ||  | `   |\   --.|  '--.
+ `-----'`--' `--'`--' `--'`--''--'`--'  `--' `----'`-----'
+""", end='')
+    print("{}   Install\n".format(C).rjust(30))
+    print("   Written by {}MOHAMED_OS{} {}(͡๏̯͡๏){}\n".format(B, C, Y, C), end='')
+    print((datetime.now().strftime("%d-%m-%Y %X")).rjust(25))
+
+
+def image():
+    return path.exists('/etc/opkg/opkg.conf')
+
+
+def check(package):
+    with open('/var/lib/opkg/status') as f:
+        for items in f.readlines():
+            if items.startswith('Package:'):
+                if findall(package, items[items.index(' '):].strip(), MULTILINE):
+                    return package
+
+
+def info():
+    try:
+        req = Request('{}version'.format(URL))
+        req.add_header(
+            'User-Agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0')
+        response = urlopen(req)
+        link = response.read().decode('utf-8')
+        return link.strip()
+    except HTTPError as e:
+        print('HTTP Error code: ', e.code)
+    except URLError as e:
+        print('URL Error: ', e.reason)
+
+
+def delete():
+    if path.isfile('/etc/enigma2/lamedb'):
+        remove('/etc/enigma2/lamedb')
+    elif path.isfile('/etc/enigma2/*list'):
+        remove('/etc/enigma2/*list')
+    elif path.isfile('/etc/enigma2/*.tv'):
+        remove('/etc/enigma2/*.tv')
+    elif path.isfile('/etc/enigma2/*.radio'):
+        remove('/etc/enigma2/*.radio')
+    elif path.isfile('/etc/tuxbox/*.xml'):
+        remove('/etc/tuxbox/*.xml')
+
+
+def main():
+
+    PathAbertis = '/etc/astra/scripts/abertis'
+    PathAstra = '/etc/astra/astra.conf'
+    Setting = 'channels_backup_user_{}.tar.gz'.format(info())
+
+    banner()
+
+    if image():
+        system('opkg update >/dev/null 2>&1')
+        for pkg in ['astra-sm', 'dvbsnoop']:
+            while not check(pkg):
+                system('clear')
+                print("   >>>>   {}Please Wait{} while we Install {}{}{} ...".format(
+                    B, C, Y, pkg, C))
+                system('opkg install {}'.format(pkg))
+
+    chdir('/tmp')
+
+    if path.isfile(Setting):
+        remove(Setting)
+
+    delete()
+
+    system('clear')
+    print("{}Downloading{} And Installing Channel {}Please Wait{} {}......{}".format(
+        Y, C, R, C, G, C))
+
+    urlretrieve("".join([URL, Setting]), filename=Setting)
+
+    with tarfile.open(Setting) as f:
+        f.extractall('/')
+        f.close()
+
+    if path.isfile(Setting):
+        remove(Setting)
+
+    urlretrieve('http://127.0.0.1/web/servicelistreload?mode=0')
+
+    if image():
+        with open('/etc/sysctl.conf', 'r+') as f:
+            if findall('net.core.rmem_default', f.read(), MULTILINE):
+                pass
+            else:
+                f.writelines("""\n
+## added for Abertis ###
+net.core.rmem_default = 16777216
+net.core.rmem_max = 16777216
+net.core.wmem_default = 16777216
+net.core.wmem_max = 16777216
+net.ipv4.udp_mem = 8388608 12582912 16777216
+net.ipv4.tcp_rmem = 4096 87380 8388608
+net.ipv4.tcp_wmem = 4096 65536 8388608
+net.ipv4.tcp_tw_recycle = 0""")
+        f.close()
+
+        if path.exists(PathAstra):
+            remove(PathAstra)
+
+        urlretrieve("".join([URL, 'astra-sm/astra.conf']), PathAstra)
+        chmod(PathAstra, 0o755)
+
+        if path.exists(PathAbertis):
+            remove(PathAbertis)
+
+        for name in ['armv7l', 'mips']:
+            if uname()[4] == name:
+                urlretrieve(
+                    "".join([URL, 'astra-sm/{}/abertis'.format(name)]), PathAbertis)
+        chmod(PathAbertis, 0o755)
+
+        system('sleep 3;init 6')
+    else:
+        system('systemctl restart enigma2')
+
+
+if __name__ == '__main__':
+    main()
+    banner()
