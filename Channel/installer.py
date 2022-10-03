447249e161dd5e1c8c8de213c7900e050f63d1a9
MOHAMED19OS <mohamed_os@gmail.com>
2022-10-03 18:49:16 +0100

    Update Script install Channel

--- Channel/installer.py
+++ Channel/installer.py
@@ -7,7 +7,7 @@ from __future__ import print_function
 import tarfile
 from datetime import datetime
 from os import chdir, chmod, remove, system, uname
-from os.path import exists, isfile
+from os.path import exists, isfile, join
 from re import MULTILINE, findall
 from sys import version_info
 
@@ -19,7 +19,6 @@ else:
 
     from urllib2 import HTTPError, Request, URLError, urlopen
 
-
 # colors
 C = "\033[0m"     # clear (end)
 R = "\033[0;31m"  # red (error)
@@ -28,138 +27,151 @@ B = "\033[0;36m"  # blue (choice)
 Y = "\033[0;33m"  # yellow (info)
 
 
-URL = 'https://raw.githubusercontent.com/MOHAMED19OS/Download/main/Channel/'
+class Setting():
+    URL_DOW = 'https://raw.githubusercontent.com/MOHAMED19OS/Download/main/Channel/'
+    url = 'https://github.com/MOHAMED19OS/Download/tree/main/Channel'
 
+    def __init__(self):
+        self.link = Setting.URL_DOW
+        self.page = Setting.url
+        self.package = []
+        self.file_info = []
+        self.date = datetime.now().strftime("%d-%m-%Y %X")
+        self.path_abertis = '/etc/astra/scripts/abertis'
+        self.path_astra = '/etc/astra/astra.conf'
 
-def banner():
+    def banner(self):
+        system('clear')
+        print(B, r"""
 
-    system('clear')
-    print(B)
-    print(r"""
- ,-----.,--.       ,---.          ,--.  ,--.       ,--.
-'  .--./|  ,---.  /  O  \ ,--,--, |  ,'.|  | ,---. |  |
-|  |    |  .-.  ||  .-.  ||      \|  |' '  || .-. :|  |
-'  '--'\|  | |  ||  | |  ||  ||  ||  | `   |\   --.|  '--.
- `-----'`--' `--'`--' `--'`--''--'`--'  `--' `----'`-----'
-""", end='')
-    print("{}   Install\n".format(C).rjust(30))
-    print("   Written by {}MOHAMED_OS{} {}(͡๏̯͡๏){}\n".format(B, C, Y, C), end='')
-    print((datetime.now().strftime("%d-%m-%Y %X")).rjust(25))
+ .d8888b.  888                                          888
+d88P  Y88b 888                                          888
+888    888 888                                          888
+888        88888b.   8888b.  88888b.  88888b.   .d88b.  888
+888        888 "88b     "88b 888 "88b 888 "88b d8P  Y8b 888
+888    888 888  888 .d888888 888  888 888  888 88888888 888
+Y88b  d88P 888  888 888  888 888  888 888  888 Y8b.     888
+ "Y8888P"  888  888 "Y888888 888  888 888  888  "Y8888  888
+""", C, end='')
+        print("   Install\n".rjust(30))
+        print("Written by {}MOHAMED_OS{} {}(͡๏̯͡๏){}\n".format(
+            B, C, Y, C).rjust(73), end='')
+        print((self.date).rjust(35))
 
+    def info(self):
+        try:
+            req = Request(self.page)
+            req.add_header(
+                'User-Agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0')
+            response = urlopen(req)
+            link = response.read().decode('utf-8')
+            self.version = findall(
+                r'href=.*?\/Channel.*?">channels_backup_user_(.*?).tar.gz', link)[0]
+            self.file_tar = findall(
+                r'href=.*?\/Channel\/channels.*?">(.*?)<', link)[0]
+            for name in [self.version, self.file_tar]:
+                self.file_info.append(name)
+        except HTTPError as e:
+            print('HTTP Error code: ', e.code)
+        except URLError as e:
+            print('URL Error: ', e.reason)
 
-def image():
-    return exists('/etc/opkg/opkg.conf')
+    def image(self):
+        return exists('/etc/opkg/opkg.conf')
 
+    def check(self):
+        self.pkg = []
+        with open('/var/lib/opkg/status') as f:
+            for items in f.readlines():
+                if items.startswith('Package:'):
+                    self.pkg.append(items[items.index(' '):].strip())
+        for p in ['astra-sm', 'dvbsnoop']:
+            if p not in self.pkg:
+                self.package.append(p)
 
-def check(package):
-    with open('/var/lib/opkg/status') as f:
-        for items in f.readlines():
-            if items.startswith('Package:'):
-                if findall(package, items[items.index(' '):].strip(), MULTILINE):
-                    return package
+    def delete(self):
+        for file in ['lamedb', '*list', '*.tv', '*.radio', '*.xml']:
+            if file != '*.xml':
+                self.path_dir = '/etc/enigma2/'
+            else:
+                self.path_dir = '/etc/tuxbox/'
+            if isfile(join(self.path_dir, file)):
+                remove(join(self.path_dir, file))
 
+    def main(self):
+        build.check()
+        build.banner()
 
-def info():
-    try:
-        req = Request('{}version'.format(URL))
-        req.add_header(
-            'User-Agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0')
-        response = urlopen(req)
-        link = response.read().decode('utf-8')
-        return link.strip()
-    except HTTPError as e:
-        print('HTTP Error code: ', e.code)
-    except URLError as e:
-        print('URL Error: ', e.reason)
+        if build.image():
+            if self.package:
+                system('opkg update >/dev/null 2>&1')
+                for pkg in self.package:
+                    system('clear')
+                    print("   >>>>   {}Please Wait{} while we Install {}{}{} ...".format(
+                        B, C, Y, pkg, C))
+                    system('opkg install {}'.format(pkg))
 
+        chdir('/tmp')
 
-def delete():
-    if isfile('/etc/enigma2/lamedb'):
-        remove('/etc/enigma2/lamedb')
-    elif isfile('/etc/enigma2/*list'):
-        remove('/etc/enigma2/*list')
-    elif isfile('/etc/enigma2/*.tv'):
-        remove('/etc/enigma2/*.tv')
-    elif isfile('/etc/enigma2/*.radio'):
-        remove('/etc/enigma2/*.radio')
-    elif isfile('/etc/tuxbox/*.xml'):
-        remove('/etc/tuxbox/*.xml')
+        if isfile(self.file_info[-1]):
+            remove(self.file_info[-1])
 
+        build.delete()
 
-def main():
+        system('clear')
+        print("{}Downloading{} And Installing Channel {}Please Wait{} {}......{}".format(
+            Y, C, R, C, G, C))
 
-    PathAbertis = '/etc/astra/scripts/abertis'
-    PathAstra = '/etc/astra/astra.conf'
-    Setting = 'channels_backup_user_{}.tar.gz'.format(info())
+        try:
+            urlretrieve("".join([self.link, self.file_info[-1]]),
+                        filename=self.file_info[-1])
+            with tarfile.open(self.file_info[-1]) as f:
+                f.extractall('/')
+            f.close()
+            if isfile(self.file_info[-1]):
+                remove(self.file_info[-1])
+        except:
+            print('No File Found')
+            exit()
 
-    banner()
+        urlretrieve('http://127.0.0.1/web/servicelistreload?mode=0')
 
-    if image():
-        system('opkg update >/dev/null 2>&1')
-        for pkg in ['astra-sm', 'dvbsnoop']:
-            while not check(pkg):
-                system('clear')
-                print("   >>>>   {}Please Wait{} while we Install {}{}{} ...".format(
-                    B, C, Y, pkg, C))
-                system('opkg install {}'.format(pkg))
+        if build.image():
+            with open('/etc/sysctl.conf', 'r+') as f:
+                if not findall('net.core.rmem_default', f.read(), MULTILINE):
+                    f.writelines("""\n
+    ## added for Abertis ###
+    net.core.rmem_default = 16777216
+    net.core.rmem_max = 16777216
+    net.core.wmem_default = 16777216
+    net.core.wmem_max = 16777216
+    net.ipv4.udp_mem = 8388608 12582912 16777216
+    net.ipv4.tcp_rmem = 4096 87380 8388608
+    net.ipv4.tcp_wmem = 4096 65536 8388608
+    net.ipv4.tcp_tw_recycle = 0""")
+            f.close()
 
-    chdir('/tmp')
+            if isfile(self.path_astra):
+                remove(self.path_astra)
+            urlretrieve(
+                "".join([self.link, self.path_astra[11:]]), self.path_astra)
+            chmod(self.path_astra, 0o755)
 
-    if isfile(Setting):
-        remove(Setting)
+            if exists(self.path_abertis):
+                remove(self.path_abertis)
 
-    delete()
+            for name in ['armv7l', 'mips']:
+                if uname()[4] == name:
+                    urlretrieve(
+                        "".join([self.link, join('astra-sm/', name, self.path_abertis[19:])]), self.path_abertis)
+            chmod(self.path_abertis, 0o755)
 
-    system('clear')
-    print("{}Downloading{} And Installing Channel {}Please Wait{} {}......{}".format(
-        Y, C, R, C, G, C))
-
-    urlretrieve("".join([URL, Setting]), filename=Setting)
-
-    with tarfile.open(Setting) as f:
-        f.extractall('/')
-        f.close()
-
-    if isfile(Setting):
-        remove(Setting)
-
-    urlretrieve('http://127.0.0.1/web/servicelistreload?mode=0')
-
-    if image():
-        with open('/etc/sysctl.conf', 'r+') as f:
-            if not findall('net.core.rmem_default', f.read(), MULTILINE):
-                f.writelines("""\n
-## added for Abertis ###
-net.core.rmem_default = 16777216
-net.core.rmem_max = 16777216
-net.core.wmem_default = 16777216
-net.core.wmem_max = 16777216
-net.ipv4.udp_mem = 8388608 12582912 16777216
-net.ipv4.tcp_rmem = 4096 87380 8388608
-net.ipv4.tcp_wmem = 4096 65536 8388608
-net.ipv4.tcp_tw_recycle = 0""")
-        f.close()
-
-        if isfile(PathAstra):
-            remove(PathAstra)
-
-        urlretrieve("".join([URL, 'astra.conf']), PathAstra)
-        chmod(PathAstra, 0o755)
-
-        if exists(PathAbertis):
-            remove(PathAbertis)
-
-        for name in ['armv7l', 'mips']:
-            if uname()[4] == name:
-                urlretrieve(
-                    "".join([URL, 'astra-sm/{}/abertis'.format(name)]), PathAbertis)
-        chmod(PathAbertis, 0o755)
-
-        system('sleep 3;init 6')
-    else:
-        system('systemctl restart enigma2')
+            system('sleep 3;init 6')
+        else:
+            system('systemctl restart enigma2')
 
 
 if __name__ == '__main__':
-    main()
-    banner()
+    build = Setting()
+    build.info()
+    build.main()
